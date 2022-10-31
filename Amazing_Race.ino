f0627404a9f06d76274118261a1357d340c548e2
YFshadaow <renzd2001@gmail.com>
2022-10-31 20:45:35 +0800

    Added buzzer and restructured code

--- Amazing_Race.ino
+++ Amazing_Race.ino
@@ -1,38 +1,141 @@
 #include <MeMCore.h>
+#include <Math.h>
 
 #define CONTROL_PIN_0 A2
 #define CONTROL_PIN_1 A3
-
-#define IR_RECEIVER_PIN 4
-#define ULTRASONIC_PIN 12 // mCore port 1 -> digital pin 12
+#define IR_RECEIVER_PIN A0
+#define ULTRASONIC_PIN 12  // mCore port 1 -> digital pin 12
 #define LDR_PIN A1
 
-#define LDR_WAIT 10
-#define LDR_INTERVAL 10 //in milliseconds'
+// LDR parameters
+#define LDR_WAIT 20
+#define LDR_INTERVAL 10  //in milliseconds'
 #define LDR_TIMES 5
 
-MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
-MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
+// Ultrasonic sensor parameters
+#define TIMEOUT 30000       // in us
+#define SPEED_OF_SOUND 340  // in m/s
+
+// Debug switch
+bool DEBUG = true;
+
+// Motors and parameters
+MeDCMotor leftMotor(M1);   // assigning leftMotor to port M1
+MeDCMotor rightMotor(M2);  // assigning RightMotor to port M2
+
 uint8_t rightSpeed = 212;
 uint8_t leftSpeed = 255;
 
+// Line follower
 MeLineFollower lineFinder(PORT_2);
 
-#define TIMEOUT 30000 // in us
-#define SPEED_OF_SOUND 340 // in m/s
+// Buzzer
+MeBuzzer buzzer;  // create the buzzer object
 
-
-bool DEBUG = true;
-
-enum Component {
-  R, G, B, IrEmmiter
+// ColorRgb struct and Color enum definition
+struct ColorRgb {
+  int r;
+  int g;
+  int b;
 };
 
-bool shouldStop() {
-  return lineFinder.readSensors() == S1_IN_S2_IN;
+enum Color {
+  Red,
+  Green,
+  Orange,
+  Purple,
+  LightBlue,
+  Black
+};
+
+//Zhengdao's values at home
+float whiteValues[] = { 741, 895, 813 };
+float blackValues[] = { 391, 530, 559 };
+ColorRgb red = { 244, 129, 115 };
+ColorRgb green = { 24, 150, 110 };
+ColorRgb orange = { 255, 170, 143 };
+ColorRgb purple = { 129, 144, 148 };
+ColorRgb lightBlue = { 95, 198, 200 };
+ColorRgb black = { 0, 0, 0 };
+
+/*
+float whiteValues[] = {813, 921, 843};
+float blackValues[] = {500, 612, 633};
+ColorRgb red = {244, 129, 115};
+ColorRgb green = {24, 150, 110};
+ColorRgb orange = {255, 170, 143};
+ColorRgb purple = {129, 144, 148};
+ColorRgb lightBlue = {95, 198, 200};
+ColorRgb black = {0, 0, 0};
+*/
+
+// To be initialized after startup
+float greyDifference[] = { 0, 0, 0 };
+
+
+// Component management
+enum Component {
+  R,
+  G,
+  B,
+  IrEmmiter
+};
+
+void enableComponent(Component c) {
+  switch (c) {
+    case R:
+      analogWrite(CONTROL_PIN_0, 255);
+      analogWrite(CONTROL_PIN_1, 255);
+      if (DEBUG) {
+        Serial.println("Enabled LED R");
+      }
+      break;
+    case G:
+      analogWrite(CONTROL_PIN_0, 255);
+      analogWrite(CONTROL_PIN_1, 0);
+      if (DEBUG) {
+        Serial.println("Enabled LED G");
+      }
+      break;
+    case B:
+      analogWrite(CONTROL_PIN_0, 0);
+      analogWrite(CONTROL_PIN_1, 255);
+
+      if (DEBUG) {
+        Serial.println("Enabled LED B");
+      }
+      break;
+    case IrEmmiter:
+      analogWrite(CONTROL_PIN_0, 0);
+      analogWrite(CONTROL_PIN_1, 0);
+      if (DEBUG) {
+        Serial.println("Enabled IR emmiter");
+      }
+      break;
+    default:  // default is R
+      analogWrite(CONTROL_PIN_0, 255);
+      analogWrite(CONTROL_PIN_1, 255);
+      if (DEBUG) {
+        Serial.println("Default: Enabled LED R");
+      }
+      break;
+  }
 }
 
-void turn(int direction) { // for direction, 0 is left, 1 is right
+// Course logic and utils
+void celebrate() {
+  // Each of the following "function calls" plays a single tone.
+  // The numbers in the bracket specify the frequency and the duration (ms)
+  buzzer.tone(392, 200);
+  buzzer.tone(523, 200);
+  buzzer.tone(659, 200);
+  buzzer.tone(784, 400);
+  buzzer.tone(659, 200);
+  buzzer.tone(784, 600);
+  buzzer.noTone();
+}
+
+void turn(int direction) {  // for direction, 0 is left, 1 is right
   if (direction == 0) {
     leftMotor.run(leftSpeed);
     rightMotor.run(rightSpeed);
@@ -65,60 +168,44 @@ void turnTwice(int direction) {
   turn(direction);
 }
 
-void enableComponent(Component c) {
-  switch (c) {
-    case R:
-      analogWrite(CONTROL_PIN_0, 255);
-      analogWrite(CONTROL_PIN_1, 255);
-      if (DEBUG) {
-        Serial.println("Enabled LED R");
-      }
-      break;
-    case G:
-      analogWrite(CONTROL_PIN_0, 255);
-      analogWrite(CONTROL_PIN_1, 0);
-      if (DEBUG) {
-        Serial.println("Enabled LED G");
-      }
-      break;
-    case B:
-      analogWrite(CONTROL_PIN_0, 0);
-      analogWrite(CONTROL_PIN_1, 255);
-      
-      if (DEBUG) {
-        Serial.println("Enabled LED B");
-      }
-      break;
-    case IrEmmiter:
-      if (DEBUG) {
-        Serial.println("Enabled IR emmiter");
-      }
-      break;
-    default: // default is R
-      analogWrite(CONTROL_PIN_0, 255);
-      analogWrite(CONTROL_PIN_1, 255);
-      if (DEBUG) {
-        Serial.println("Default: Enabled LED R");
-      }
-      break;
-  }
+bool shouldStop() {
+  return lineFinder.readSensors() == S1_IN_S2_IN;
 }
 
 int getLdrReading(int times) {
-  delay(LDR_WAIT);   
+  delay(LDR_WAIT);
   //find the average reading for the requested number of times of scanning LDR
   int total = 0;
   //take the reading as many times as requested and add them up
-  for(int i = 0; i < times; i += 1){
-     total += analogRead(LDR_PIN);
-     delay(LDR_INTERVAL);
+  for (int i = 0; i < times; i += 1) {
+    int reading = analogRead(LDR_PIN);
+    total += reading;
+    /*
+    if (DEBUG) {
+      Serial.print("The ");
+      Serial.print(i);
+      Serial.print("th reading is ");
+      Serial.println(reading);
+    }
+    */
+    delay(LDR_INTERVAL);
   }
   //calculate the average and return it
-  return total/times;
+  int result = (int)(total / times);
+  if (DEBUG) {
+    Serial.print("LDR reading is ");
+    Serial.println(result);
+  }
+  return result;
 }
 
-float getDistanceIr () {
-  
+int getIntensityIr() {
+  delayMicroseconds(2);
+  enableComponent(IrEmmiter);
+  delayMicroseconds(10);
+  int intensity = analogRead(IR_RECEIVER_PIN);
+  enableComponent(R);
+  return intensity;
 }
 
 float getDistanceUltraSonic() {
@@ -138,16 +225,131 @@ float getDistanceUltraSonic() {
   }
 }
 
+void doAction(Color c) {
+  switch (c) {
+    case Black:
+      Serial.println("Detected black");
+      break;
+    case Red:
+      Serial.println("Detected red");
+      break;
+    case Green:
+      Serial.println("Detected green");
+      break;
+    case Orange:
+      Serial.println("Detected orange");
+      break;
+    case Purple:
+      Serial.println("Detected purple");
+      break;
+    case LightBlue:
+      Serial.println("Detected LightBlue");
+      break;
+    default:
+      Serial.println("No color detected, is there a bug?");
+  }
+}
+
+
+// Color related stuff
+
+float normalizeProportion(float n) {
+  float result = n;
+  if (n < 0) {
+    result = 0;
+  } else if (n > 1) {
+    result = 1;
+  }
+  // Squaring result to produce a y = x ^ 2 curve.
+  //result = result * result;
+  return result;
+}
+
+float getColorDifference(struct ColorRgb colorA, struct ColorRgb colorB) {
+  //quadratic approach
+  return pow(colorA.r - colorB.r, 2) + pow(colorA.g - colorB.g, 2) + pow(colorA.b - colorB.b, 2);
+  //linear approach
+  //return fabs(colorA.r - colorB.r) + fabs(colorA.g - colorB.g) + fabs(colorA.b - colorB.b);
+}
+
+Color colorRgbToColor(struct ColorRgb c) {
+  Color result = Black;
+  float minDiff = getColorDifference(c, black);
+  float currentDiff = getColorDifference(c, red);
+  if (currentDiff < minDiff) {
+    result = Red;
+    minDiff = currentDiff;
+  }
+  currentDiff = getColorDifference(c, red);
+  if (currentDiff < minDiff) {
+    result = Red;
+    minDiff = currentDiff;
+  }
+  currentDiff = getColorDifference(c, green);
+  if (currentDiff < minDiff) {
+    result = Green;
+    minDiff = currentDiff;
+  }
+  currentDiff = getColorDifference(c, orange);
+  if (currentDiff < minDiff) {
+    result = Orange;
+    minDiff = currentDiff;
+  }
+  currentDiff = getColorDifference(c, purple);
+  if (currentDiff < minDiff) {
+    result = Purple;
+    minDiff = currentDiff;
+  }
+  currentDiff = getColorDifference(c, lightBlue);
+  if (currentDiff < minDiff) {
+    result = LightBlue;
+  }
+  return result;
+}
+
+struct ColorRgb getColor() {
+  struct ColorRgb color;
+  enableComponent(R);
+  float rProportion = ((float)(getLdrReading(LDR_TIMES)) - blackValues[0]) / (greyDifference[0]);
+  rProportion = normalizeProportion(rProportion);
+  color.r = rProportion * 255;
+  enableComponent(G);
+  float gProportion = ((float)(getLdrReading(LDR_TIMES)) - blackValues[1]) / (greyDifference[1]);
+  gProportion = normalizeProportion(gProportion);
+  color.g = gProportion * 255;
+  enableComponent(B);
+  float bProportion = ((float)(getLdrReading(LDR_TIMES)) - blackValues[2]) / (greyDifference[2]);
+  bProportion = normalizeProportion(bProportion);
+  color.b = bProportion * 255;
+  enableComponent(R);  // change to default state
+  if (DEBUG) {
+    Serial.print("Normalized R value is ");
+    Serial.println(color.r);
+    Serial.print("Normalized G value is ");
+    Serial.println(color.g);
+    Serial.print("Normalized B value is ");
+    Serial.println(color.b);
+  }
+  return color;
+}
+
 void setup() {
   //ultrasonic initialization
   pinMode(ULTRASONIC_PIN, OUTPUT);
   digitalWrite(ULTRASONIC_PIN, LOW);
 
+  //IR initialization
+  pinMode(IR_RECEIVER_PIN, INPUT);
+
   //control pins initialization
   enableComponent(R);
 
-  Serial.begin(9600);
+  //initialize grey difference
+  for (int i = 0; i < 3; i += 1) {
+    greyDifference[i] = whiteValues[i] - blackValues[i];
+  }
 
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -164,12 +366,23 @@ void loop() {
     }
   }
 
-  if (shouldStop()) {
-    delay(1000);
-    enableComponent(R);
-    delay(1000);
-    enableComponent(G);
-    delay(1000);
-    enableComponent(B);
+  delay(500);
+
+  int intensity = getIntensityIr();
+  if (DEBUG) {
+    Serial.print("Intensity is ");
+    Serial.println(intensity);
   }
+  delay(500);
+
+  struct ColorRgb color = getColor();
+  doAction(colorRgbToColor(color));
+
+  delay(500);
+  // test line tracer
+  Serial.println(shouldStop());
+  delay(500);
+
+  celebrate();
+  delay(500);
 }
